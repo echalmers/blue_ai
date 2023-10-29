6a467821cfa36b00a6454171a65ea4c0c4d68049
echalmers <echalmers@mtroyal.ca>
2023-10-29 08:42:09 -0600

    update weight measurements

--- blue_ai/scripts/weight_update.py
+++ blue_ai/scripts/weight_update.py
@@ -1,5 +1,7 @@
-from blue_ai.scripts.train_dqn import load_trial, Image2VecWrapper, DQN
+from blue_ai.scripts.train_agents import save_trial
 from blue_ai.envs.transient_goals import TransientGoals
+from blue_ai.envs.custom_wrappers import Image2VecWrapper
+from blue_ai.agents.agent_classes import HealthyAgent, SpineLossDepression
 from torch import nn
 import matplotlib.pyplot as plt
 
@@ -7,6 +9,69 @@ import os
 import numpy as np
 
 
+for agent in [HealthyAgent(), SpineLossDepression()]:
+
+    env = Image2VecWrapper(TransientGoals(render_mode='none'))
+    state, _ = env.reset()
+
+    steps = 15000
+    steps_this_episode = 0
+    weight_changes = []
+    losses = []
+    reward_accumulator = 0
+    reward_history = []
+    change_per_loss = []
+
+    for step in range(steps):
+        steps_this_episode += 1
+
+        # get & execute action
+        action = agent.select_action(np.expand_dims(state, 0))
+        new_state, reward, done, _, _ = env.step(action)
+        reward_accumulator += reward
+
+        # use this experience to update agent
+        old_weights = np.concatenate(
+            (agent.policy_net[1].weight.detach().flatten().numpy(), agent.policy_net[3].weight.detach().flatten().numpy())
+        )
+
+        loss = agent.update(state, action, reward, new_state, done=False)
+
+        if loss is not None:
+            new_weights = np.concatenate(
+                (agent.policy_net[1].weight.detach().flatten().numpy(), agent.policy_net[3].weight.detach().flatten().numpy())
+            )
+            changes = new_weights - old_weights
+            weight_changes.append(np.mean(np.abs(changes)))
+            losses.append(loss)
+            change_per_loss.append(weight_changes[-1] / losses[-1])
+            reward_history.append(reward_accumulator)
+            reward_accumulator = 0
+
+        # reset environment if done (ideally env would do this itself)
+        if done or steps_this_episode > 500:
+            state, _ = env.reset()
+            steps_this_episode = 0
+        else:
+            state = new_state
+
+    plt.subplot(3, 1, 1)
+    plt.plot(weight_changes)
+    plt.subplot(3, 1, 2)
+    plt.plot(change_per_loss)
+    plt.subplot(3, 1, 3)
+    plt.plot(np.array(reward_history).cumsum())
+
+plt.show()
+
+
+
+
+
+
+
+
+exit()
 for dropout in [0, 50]:
 
 
