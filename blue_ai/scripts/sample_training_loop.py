cf1e192889654b4debdda4bda7578fbc3867a017
Ezzidean <ezzidean1@gmail.com>
2023-11-28 18:51:37 -0700

    Completed slope function with addition slop plot for reference

--- blue_ai/scripts/sample_training_loop.py
+++ blue_ai/scripts/sample_training_loop.py
@@ -6,33 +6,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
-from time import time as epochtime
 
 
-rewards_table = pd.DataFrame(columns=['step', 'cumulative reward'])
-goals_table = pd.DataFrame(columns=['goal type', 'count'])
+rewards_table = pd.DataFrame(columns=["step", "cumulative reward"])
+goals_table = pd.DataFrame(columns=["goal type", "count"])
+
+def slope_calc(rewards, a, b):
+    return np.sum(np.diff(np.cumsum(rewards[a : b + 1]))) / (b - a)
 
 for _ in range(1):
     # a multi-layer network
     multilayer = nn.Sequential(
-        nn.Flatten(1, -1),
-        nn.Linear(100, 10),
-        nn.Tanh(),
-        nn.Linear(10, 3)
+        nn.Flatten(1, -1), nn.Linear(100, 10), nn.Tanh(), nn.Linear(10, 3)
     )
 
     # instantiate the agent
     agent = DQN(
-            network=multilayer,
-            input_shape=(4, 5, 5),
-            replay_buffer_size=10000,
-            update_frequency=5,
-            lr=0.005,
-            sync_frequency=25,
-            gamma=0.2,  # discount factor
-            epsilon=0.05,  # random exploration rate
-            batch_size=1500,
-            weight_decay=0,  # we've been using 2.5e-3 for depression
+        network=multilayer,
+        input_shape=(4, 5, 5),
+        replay_buffer_size=10000,
+        update_frequency=5,
+        lr=0.005,
+        sync_frequency=25,
+        gamma=0.2,  # discount factor
+        epsilon=0.05,  # random exploration rate
+        batch_size=1500,
+        weight_decay=0,  # we've been using 2.5e-3 for depression
     )
 
     # 3 possible other ways to simulate depression:
@@ -40,23 +39,33 @@ for _ in range(1):
     # - shift down reward_prediction_error (also on line 146) by subtracting instead of multiplying
     # - use a smaller discount factor (this *should* be the same or similar to the downscaling, could try to match the effects)
 
-
     # create the environment
-    env = Image2VecWrapper(TransientGoals(render_mode='none', transient_reward=0.25, termination_reward=1))  # set render mode to "human" to see the agent moving around
+    env = Image2VecWrapper(
+        TransientGoals(render_mode="none", transient_reward=0.25, termination_reward=1)
+    )  # set render mode to "human" to see the agent moving around
     state, _ = env.reset()
 
     # set up an array and other variables to store results
-    STEPS = 1_000
+    STEPS = 5_000
+    #range of slope
+    a = 2000
+    b = 3000
+    steps_this_episode = 0
     rewards = np.zeros(STEPS)
     num_required_goals = 0
     num_optional_goals = 0
     num_lava = 0
-
-
+    
+    #Slope calc 
+    print(slope_calc(rewards,a,b))
+    
+    
     # training loop
     for step in range(STEPS):
-        if step % (STEPS/10) == 0:
-             print(f"\r{step}", end="")
+        if step % (STEPS // 10) == 0:
+            print(step)
+
+        steps_this_episode += 1
 
         # get & execute action
         action = agent.select_action(np.expand_dims(state, 0))
@@ -69,12 +78,44 @@ for _ in range(1):
         num_lava += reward < 0
 
         # update the agent
-        agent.update(state=state, new_state=new_state, reward=reward, done=done, action=action)
+        agent.update(
+            state=state, new_state=new_state, reward=reward, done=done, action=action
+        )
 
-    rewards_table = pd.concat((rewards_table, pd.DataFrame({'step': np.arange(STEPS), 'cumulative reward': rewards.cumsum()})), ignore_index=True)
-    goals_table = pd.concat((goals_table, pd.DataFrame({'goal type': ['required', 'optional', 'lava'], 'count': [num_required_goals, num_optional_goals, num_lava]})))
+        # reset the environment if goal reached
+        if done or steps_this_episode > 500:
+            state, _ = env.reset()
+            steps_this_episode = 0
+        else:
+            state = new_state
 
-sns.lineplot(rewards_table, x='step', y='cumulative reward', n_boot=5)
-plt.savefig(f"data\\plot-{int(epochtime())}.png")
-sns.barplot(goals_table, x='goal type', y='count', n_boot=5)
-plt.savefig(f"data\\graph-{int(epochtime())}.png")
+    rewards_table = pd.concat(
+        (
+            rewards_table,
+            pd.DataFrame(
+                {"step": np.arange(STEPS), "cumulative reward": rewards.cumsum()}
+            ),
+        )
+        ignore_index=True,
+    )
+    goals_table = pd.concat(
+        (
+            goals_table,
+            pd.DataFrame(
+                {
+                    "goal type": ["required", "optional", "lava"],
+                    "count": [num_required_goals, num_optional_goals, num_lava],
+                }
+            ),
+        )
+    )
+
+
+
+
+
+sns.lineplot(np.diff(np.cumsum(rewards)))
+sns.lineplot(rewards_table, x="step", y="cumulative reward", n_boot=5)
+plt.figure()
+sns.barplot(goals_table, x="goal type", y="count", n_boot=5)
+plt.show()
