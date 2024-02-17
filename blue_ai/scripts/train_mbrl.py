50bb79016f258c09d0ee94ddcd884eab98995b95
echalmers <echalmers@mtroyal.ca>
2024-02-17 12:14:02 -0700

    remove cached files

---
+++ blue_ai/scripts/train_mbrl.py
@@ -0,0 +1,24 @@
+from train_agents import run_trial
+from blue_ai.agents.tabular.mbrl import MBRL
+from blue_ai.envs.custom_wrappers import Image2FlatVecWrapper
+from blue_ai.envs.transient_goals import TransientGoals
+
+import pickle
+import os
+
+agent_save_path = os.path.join('.', 'data', 'mbrl.pkl')
+
+if os.path.exists(agent_save_path):
+    agent = MBRL.load(agent_save_path, actions=[0,1,2])
+else:
+    agent = MBRL(actions=[0,1,2], max_value_iterations=1000, theta_threshold=0.001, epsilon=0.25, q_default=1)
+
+agent.max_value_iterations = 100
+env = Image2FlatVecWrapper(TransientGoals(render_mode='human', transient_reward=0.25, termination_reward=1))  # set render mode to "human" to see the agent moving around
+
+for i in range(10_000 // 500):
+    print(i)
+    results, agent, env = run_trial(agent=agent, env=env, steps=500)
+    agent.save(agent_save_path)
+
+print(results)
