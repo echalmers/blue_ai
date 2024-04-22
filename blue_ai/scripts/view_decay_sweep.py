757c84f45d8e55a1e521e9e66ab16d51f4726302
echalmers <echalmers@mtroyal.ca>
2024-04-21 18:50:35 -0600

    add decay-sweep and simple environments

--- blue_ai/scripts/view_decay_sweep.py
+++ blue_ai/scripts/view_decay_sweep.py
@@ -6,8 +6,12 @@ import os
 results = pd.read_csv(os.path.join('.', 'data', 'decay_sweep.csv'))
 results['reward'] *= 100
 print(results)
-sns.lineplot(results, x='decay', y='reward')
+plt.figure(figsize=(8, 4))
+sns.lineplot(results, x='decay', y='reward', palette=['grey'])
 plt.xscale('log')
 plt.xlabel('weight decay (λ)')
 plt.ylabel('average reward per 100 steps')
+plt.title('performance as weight decay increases')
+plt.tight_layout()
+plt.savefig('img/decay_sweep.png', dpi=300)
 plt.show()
--- blue_ai/scripts/view_simple_env_comparison.py
+++ blue_ai/scripts/view_simple_env_comparison.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 import os
 
 results = pd.read_csv(os.path.join('.', 'data', 'simple_env_comparison.csv'))
+results['agent'] = results['agent'].replace({'HealthyAgent': 'healthy', 'SpineLossDepression': 'simluated\nspine loss'})
 results['reward'] *= 100
 print(results)
-sns.catplot(data=results, kind='bar', x='env', y='reward', hue='agent')
+p = sns.catplot(data=results, kind='bar', x='env', y='reward', hue='agent', palette=['skyblue', 'salmon'], legend_out=False)
+p.fig.set_size_inches(9, 4)
+# sns.move_legend(p, 'upper left')
 plt.xlabel('environment')
 plt.ylabel('average reward per 100 steps')
+plt.title('performance in full-complexity and simple environments')
+plt.tight_layout()
+plt.savefig('img/simple_env_comparison.png', dpi=300)
 plt.show()
