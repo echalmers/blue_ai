4da11d43436892e10eb2b3473e539a38fcfdc86c
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-05-13 12:35:57 -0600

    fixed missing n_trials const

--- blue_ai/scripts/constants.py
+++ blue_ai/scripts/constants.py
@@ -2,8 +2,11 @@ from pathlib import Path
 
 _CURRENT_DIR = Path(__file__).parent
 
+N_TRIALS = 10
+
 FIGURE_PATH = _CURRENT_DIR / "img"
 DATA_PATH = _CURRENT_DIR / "data"
+
 
 DATA_PATH.mkdir(exist_ok=True)
 DATA_PATH.mkdir(exist_ok=True)
