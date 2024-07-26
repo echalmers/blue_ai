bc379902157e43d51c01818ba4a7ee818aea421d
echalmers <echalmers@mtroyal.ca>
2024-07-26 11:28:30 -0600

    remove old code

--- blue_ai/scripts/window_of_plasticity/view_rehabilitation.py
+++ blue_ai/scripts/window_of_plasticity/view_rehabilitation.py
@@ -11,8 +11,6 @@ df = pd.read_csv(DATA_PATH / 'rehabilitation_tests.csv', index_col=None)
 df['reward_normalized'] = df['reward'] / df['reward_healthy']
 
 # rolling avg
-# df['reward_avg_normalized'] = df['reward_normalized'].rolling(1000).mean()
-# df['reward_avg'] = df['reward'].rolling(1000).mean()
 df['reward_avg'] = df.groupby(['agent', 'trial_id'])['reward'].transform(lambda x: x.rolling(2000).mean())
 df['reward_avg_normalized'] = df.groupby(['agent', 'trial_id'])['reward_normalized'].transform(lambda x: x.rolling(2000).mean())
 
@@ -22,15 +20,14 @@ df.drop(df[df['step'] % 20 != 0].index, inplace=True)
 
 ax1 = plt.subplot(1, 3, 1)
 sns.lineplot(data=df[df['agent'] == 'SpineLossDepression'], x='step', y='reward_avg', hue='path', errorbar='sd')
+plt.grid()
 
 plt.subplot(1, 3, 2, sharex=ax1)
 sns.lineplot(data=df[df['agent'] == 'SpineLossDepression'], x='step', y='reward_avg_normalized', hue='path', errorbar='sd')
+plt.grid()
 
 plt.subplot(1, 3, 3, sharex=ax1)
 sns.lineplot(data=df[df['agent'] == 'SpineLossDepression'], x='step', y='cumulative_reward', hue='path', errorbar='sd')
-
 plt.grid()
-
-
 
 plt.show()
