5eaa8fb2336c481a9041865a3d672757ca5b7e8b
benitomano <benito.manocchio@gmail.com>
2025-08-21 14:38:40 -0600

    style adjustments

--- blue_ai/scripts/ptsd/experiments/execute.py
+++ blue_ai/scripts/ptsd/experiments/execute.py
@@ -23,12 +23,20 @@ from blue_ai.scripts.ptsd.post_ptsd import post_ptsd
 
 # before using this script, get yourself familiar with the README in the ptsd folder
 def main():
+    # -- HYPERPARAMETERS --
+    iter_per_trial = 1_000
+    show_plots = True
+    trauma_penalty = -100
+    n_trauma_updates = 1
+    n_exposure_updates = 10
+    
 
     # -- CREATE THE DIRECTORY --
     if len(sys.argv) < 2:
         raise ValueError("Please specify how you wanna name the directory in which the results get saved.")
     folder_path = DATA_PATH / sys.argv[1]
     folder_path.mkdir(parents=True, exist_ok=True)
+
 
     # -- AGENTS, ENVIRONMENTS & NETWORK--
     network = nn.Sequential(
@@ -59,7 +67,7 @@ def main():
     trauma_env = Image2VecWrapper(
                 TransientGoals(
                     render_mode="none", transient_reward=0.25, termination_reward=1, 
-                    agent_start_pos=(3,1), n_transient_obstacles=1, transient_penalty=-100, 
+                    agent_start_pos=(3,1), n_transient_obstacles=1, transient_penalty = trauma_penalty, 
                     transient_locations=[[1,4],[4,2],[5,1]], transient_obstacles=[[4,1]],
                     wall_locations =[[3,2],[3,3],[3,4],[3,5],[3,6]]
                 )
@@ -71,40 +79,42 @@ def main():
     post_trauma_env.unwrapped.n_transient_obstacles = 0
     post_trauma_env.unwrapped.transient_obstacles = None
     save_env(post_trauma_env, 'environment_after_trauma_experience', folder_path)
-    
-    # -- OTHER HYPERPARAMETERS --
-    iter_per_trial = 1_000
-    show_plots = True
+
 
     # -- TRAIN THE AGENTS --
     train_agents(agents, learning_envs, iter_per_trial, folder_path)
 
+
     # -- PLOT AND SAVE THE PERFORMANCE --
     view_performance(folder_path, "in_learning_env", show_plots)
 
+
     # -- INDUCE THE TRAUMATIV EVENT TO ALL AGENTS --
-    induce_traumatic_event(folder_path, trauma_env)
+    induce_traumatic_event(folder_path, trauma_env, n_trauma_updates)
+
 
     # -- INVESTIGATE THE Q-VALUES IN THE TRAUMA ENV BUT WITHOUT THE HAZARD --
     # Das gefällt mir noch nicht so gut, wäre schöner, wenn es einfach ein plot wäre
     investigate_Qvalues(folder_path, post_trauma_env, show_plots, trauma = False,)
     investigate_Qvalues(folder_path, post_trauma_env, show_plots, trauma = True)
 
+
     # -- TRAIN THE MODLES WHICH RECONSTRUCT THE VISUAL INPUT --
     train_interpretation_models(folder_path, trauma = False)
     train_interpretation_models(folder_path, trauma = True)
 
-    # -- RENDER THE RECONSTRUCTIONS --
+
+    # -- RECONSTRUCT WHAT THE AGENTS SEE --
     if show_plots:
         view_reconstruction(folder_path, post_trauma_env, trauma = False, mode = 'interactive')
         view_reconstruction(folder_path, post_trauma_env, trauma = True, mode = 'interactive')
     
-    # -- ANALYSE THE RECONSTRUCTIONS --
     view_reconstruction(folder_path, post_trauma_env, trauma = False, mode = 'statistical')
     view_reconstruction(folder_path, post_trauma_env, trauma = True, mode = 'statistical')
 
+
     # -- EXPOSURE THERAPY --
-    post_ptsd(folder_path, post_trauma_env, 5)
+    post_ptsd(folder_path, post_trauma_env, n_exposure_updates)
 
 
 
